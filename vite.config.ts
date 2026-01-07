3681e30b126ca2ac38b4096ddf6fb9904e79babf
Mohaymen Mahomed <xxmohaymenxx@gmail.com>
2026-01-07 20:03:27 +0400

    1

--- vite.config.ts
+++ vite.config.ts
@@ -21,7 +21,6 @@ export default defineConfig(({ mode }: ConfigEnv) => {
       host: "::",
       port: 8080,
       headers: {
-        'Content-Security-Policy': `default-src 'self'; script-src 'self' 'nonce-${nonce}' 'strict-dynamic' https: 'unsafe-inline' https://www.googletagmanager.com https://www.google-analytics.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; img-src 'self' data: https:; font-src 'self' https://fonts.gstatic.com; connect-src 'self' https://www.google-analytics.com; frame-src 'self';`,
         'X-Content-Type-Options': 'nosniff',
         'X-Frame-Options': 'SAMEORIGIN',
         'X-XSS-Protection': '1; mode=block',
