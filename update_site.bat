a42f943d44ef906f98f4ab4e6ff128b9e2855c15
Mohaymen Mahomed <xxmohaymenxx@gmail.com>
2026-03-10 15:43:02 +0400

    1

--- update_site.bat
+++ update_site.bat
@@ -7,7 +7,7 @@ REM Ask for commit message
 set /p msg="Enter commit message: "
 
 REM Navigate to your project folder
-cd /d "C:\Users\mohaymen\Desktop\emrs site source"
+cd /d "C:\Users\mohaymen\Downloads\swift-aid-revive-main\swift-aid-revive-main"
 
 REM Stage all changes
 git add .
@@ -15,7 +15,7 @@ git add .
 REM Commit with your message
 git commit -m "%msg%"
 
-REM Push to GitHub
-git push
+REM Push to GitHub and set upstream if needed
+git push --set-upstream origin master
 
 pause
